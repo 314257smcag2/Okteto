b1c36df6b3a6f3f6f0eae5bda67f7d26a2e2ff86
wwkiyyx <670405621@qq.com>
2021-08-14 14:19:31 +0000

    alive

--- app.py
+++ app.py
@@ -1,24 +1,65 @@
 import datetime
 import time
 import socket
+import urllib.request
+import _thread
+
+
+def alive():
+    while 1:
+        time.sleep(100)
+        try:
+            print(urllib.request.urlopen("https://app-wwkiyyx.cloud.okteto.net").read().decode('utf-8'))
+        except Exception as ex:
+            print(datetime.datetime.now(), ex)
+        time.sleep(100)
+        try:
+            print(urllib.request.urlopen("https://app-a-wwkiyyx.cloud.okteto.net").read().decode('utf-8'))
+        except Exception as ex:
+            print(datetime.datetime.now(), ex)
+        time.sleep(100)
+        try:
+            print(urllib.request.urlopen("https://app-b-wwkiyyx.cloud.okteto.net").read().decode('utf-8'))
+        except Exception as ex:
+            print(datetime.datetime.now(), ex)
+        time.sleep(100)
+        try:
+            print(urllib.request.urlopen("https://app-c-wwkiyyx.cloud.okteto.net").read().decode('utf-8'))
+        except Exception as ex:
+            print(datetime.datetime.now(), ex)
+        time.sleep(100)
+        try:
+            print(urllib.request.urlopen("https://app-d-wwkiyyx.cloud.okteto.net").read().decode('utf-8'))
+        except Exception as ex:
+            print(datetime.datetime.now(), ex)
+        time.sleep(100)
+
+
+def client(clientsocket, addr):
+    try:
+        data = clientsocket.recv(10240).decode('utf-8')
+        print(datetime.datetime.now(), addr, data)
+        response_start_line = "HTTP/1.1 200 OK\r\n"
+        response_body = "<h1>hello</h1>"
+        response = response_start_line + "\r\n" + response_body
+        clientsocket.send(bytes(response, "utf-8"))
+        clientsocket.close()
+    except Exception as ex:
+        print(datetime.datetime.now(), addr, ex)
+
+
 print(datetime.datetime.now(), "   start")
+_thread.start_new_thread(alive, ())
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serversocket.bind(('0.0.0.0', 8989))
 serversocket.listen(100)
 print(datetime.datetime.now(), "   0.0.0.0:8989")
 while 1:
-    clientsocket,addr = serversocket.accept()
+    clientsocket, addr = serversocket.accept()
     print(datetime.datetime.now(), addr)
-    while 1:
-        try:
-            data = clientsocket.recv(1024).decode('utf-8')
-            print(datetime.datetime.now(),addr,data)
-            msg = "hello"
-            clientsocket.send(msg.encode('utf-8'))
-        except:
-            print(datetime.datetime.now(),addr,"exception")
-            break
-    clientsocket.close()
-    print(datetime.datetime.now(), "   close")
+    _thread.start_new_thread(client, (clientsocket, addr))
     time.sleep(1)
 print(datetime.datetime.now(), "   exit")
+
+
+
