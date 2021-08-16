ce7718f5b450f412a3c118c11179bcae2adf8a71
wwk i yyx <wwkiyyx@qq.com>
2021-08-16 03:54:12 +0000

    format

--- app.py
+++ app.py
@@ -9,27 +9,37 @@ def alive():
     while 1:
         time.sleep(100)
         try:
-            print(urllib.request.urlopen("https://app-wwkiyyx.cloud.okteto.net").read().decode('utf-8'))
+            url = "https://app-wwkiyyx.cloud.okteto.net"
+            print(datetime.datetime.now(), url)
+            print(urllib.request.urlopen(url).read().decode('utf-8'))
         except Exception as ex:
             print(datetime.datetime.now(), ex)
         time.sleep(100)
         try:
-            print(urllib.request.urlopen("https://app-a-wwkiyyx.cloud.okteto.net").read().decode('utf-8'))
+            url = "https://app-a-wwkiyyx.cloud.okteto.net"
+            print(datetime.datetime.now(), url)
+            print(urllib.request.urlopen(url).read().decode('utf-8'))
         except Exception as ex:
             print(datetime.datetime.now(), ex)
         time.sleep(100)
         try:
-            print(urllib.request.urlopen("https://app-b-wwkiyyx.cloud.okteto.net").read().decode('utf-8'))
+            url = "https://app-b-wwkiyyx.cloud.okteto.net"
+            print(datetime.datetime.now(), url)
+            print(urllib.request.urlopen(url).read().decode('utf-8'))
         except Exception as ex:
             print(datetime.datetime.now(), ex)
         time.sleep(100)
         try:
-            print(urllib.request.urlopen("https://app-c-wwkiyyx.cloud.okteto.net").read().decode('utf-8'))
+            url = "https://app-c-wwkiyyx.cloud.okteto.net"
+            print(datetime.datetime.now(), url)
+            print(urllib.request.urlopen(url).read().decode('utf-8'))
         except Exception as ex:
             print(datetime.datetime.now(), ex)
         time.sleep(100)
         try:
-            print(urllib.request.urlopen("https://app-d-wwkiyyx.cloud.okteto.net").read().decode('utf-8'))
+            url = "https://app-d-wwkiyyx.cloud.okteto.net"
+            print(datetime.datetime.now(), url)
+            print(urllib.request.urlopen().read().decode('utf-8'))
         except Exception as ex:
             print(datetime.datetime.now(), ex)
         time.sleep(100)
@@ -38,7 +48,7 @@ def alive():
 def client(clientsocket, addr):
     try:
         data = clientsocket.recv(10240).decode('utf-8')
-        print(datetime.datetime.now(), addr, data)
+        print(datetime.datetime.now(), data)
         response_start_line = "HTTP/1.1 200 OK\r\n"
         response_body = "<h1>hello</h1>"
         response = response_start_line + "\r\n" + response_body
@@ -48,16 +58,16 @@ def client(clientsocket, addr):
         print(datetime.datetime.now(), addr, ex)
 
 
-print(datetime.datetime.now(), "   start")
+print(datetime.datetime.now(), "start")
 _thread.start_new_thread(alive, ())
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serversocket.bind(('0.0.0.0', 8989))
 serversocket.listen(100)
-print(datetime.datetime.now(), "   0.0.0.0:8989")
+print(datetime.datetime.now(), "at 0.0.0.0:8989")
 while 1:
     clientsocket, addr = serversocket.accept()
     print(datetime.datetime.now(), addr)
     _thread.start_new_thread(client, (clientsocket, addr))
     time.sleep(1)
-print(datetime.datetime.now(), "   exit")
+print(datetime.datetime.now(), "exit")
 
--- app1.py
+++ app1.py
@@ -7,7 +7,7 @@ import _thread
 def client(clientsocket, addr):
     try:
         data = clientsocket.recv(10240).decode('utf-8')
-        print(datetime.datetime.now(), addr, data)
+        print(datetime.datetime.now(), data)
         response_start_line = "HTTP/1.1 200 OK\r\n"
         response_body = "<h1>hello</h1>"
         response = response_start_line + "\r\n" + response_body
@@ -17,14 +17,14 @@ def client(clientsocket, addr):
         print(datetime.datetime.now(), addr, ex)
 
 
-print(datetime.datetime.now(), "   start")
+print(datetime.datetime.now(), "start")
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serversocket.bind(('0.0.0.0', 80))
 serversocket.listen(100)
-print(datetime.datetime.now(), "   0.0.0.0:8989")
+print(datetime.datetime.now(), "at 0.0.0.0:80")
 while 1:
     clientsocket, addr = serversocket.accept()
     print(datetime.datetime.now(), addr)
     _thread.start_new_thread(client, (clientsocket, addr))
     time.sleep(1)
-print(datetime.datetime.now(), "   exit")
+print(datetime.datetime.now(), "exit")
