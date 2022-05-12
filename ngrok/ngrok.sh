23004aa686bb432d960ced52f2797ccb48ec9b8a
wangwenkai <670405621@qq.com>
2022-05-12 03:41:07 +0000

    xiaomiqiu ngrok

---
+++ ngrok/ngrok.sh
@@ -0,0 +1,3 @@
+chmod u+x ./ngrok
+./ngrok config add-authtoken 1g4REG4jBZwQPY0EfOfvXkP8tbv_4VNpJLsc9WsV1VZQ5Ara
+./ngrok http 80
---
+++ ngrok/xiaomiqiu.sh
@@ -0,0 +1,2 @@
+chmod u+x ./xiaomiqiu
+./xiaomiqiu -config xiaomiqiu.conf -log=xiaomiqiu.log -log-level=info start-all
